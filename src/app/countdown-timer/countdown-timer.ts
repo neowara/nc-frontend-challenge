f0abdf0c2ca6747980a74f609b3626743244d328
Christian Toledo <mugidota@hotmail.com>
2025-06-06 00:43:48 +0200

    feat: add countdown timer, event form, and fit text components with basic structure

---
+++ src/app/countdown-timer/countdown-timer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-timer',
+  imports: [],
+  templateUrl: './countdown-timer.html',
+  styleUrl: './countdown-timer.scss'
+})
+export class CountdownTimer {
+
+}
---
+++ src/app/shared/fit-text/fit-text.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fit-text',
+  imports: [],
+  templateUrl: './fit-text.html',
+  styleUrl: './fit-text.scss'
+})
+export class FitText {
+
+}
