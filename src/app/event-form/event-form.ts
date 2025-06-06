394ac70f5f52415ae777c8e81f0f86f869cec61a
Christian Toledo <mugidota@hotmail.com>
2025-06-06 14:16:10 +0200

    feat: implement event form component with input bindings for event name and date

--- src/app/event-form/event-form.ts
+++ src/app/event-form/event-form.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-event-form',
+  standalone: true,
   imports: [],
   templateUrl: './event-form.html',
-  styleUrl: './event-form.scss'
+  styleUrl: './event-form.scss',
 })
-export class EventForm {
+export class EventFormComponent {
+  @Input() eventName = '';
+  @Input() eventDate = '';
+  @Output() eventNameChange = new EventEmitter<string>();
+  @Output() eventDateChange = new EventEmitter<string>();
 
+  onNameInput(event: Event) {
+    const value = (event.target as HTMLInputElement)?.value || '';
+    this.eventNameChange.emit(value);
+  }
+
+  onDateInput(event: Event) {
+    const value = (event.target as HTMLInputElement)?.value || '';
+    this.eventDateChange.emit(value);
+  }
 }
