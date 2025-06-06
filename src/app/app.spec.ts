1f17288840285380f3214a5af9c266db4cd9eff0
Christian Toledo <mugidota@hotmail.com>
2025-06-06 19:37:46 +0200

    test: update app component test to verify countdown timer rendering

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,12 +14,12 @@ describe('App', () => {
     expect(app).toBeTruthy()
   })
 
-  it('should render title', () => {
+  it('should render countdown timer app', () => {
     const fixture = TestBed.createComponent(App)
     fixture.detectChanges()
     const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, temp-app',
-    )
+    expect(compiled.querySelector('.main')).toBeTruthy()
+    expect(compiled.querySelector('app-countdown-timer')).toBeTruthy()
+    expect(compiled.querySelector('app-event-form')).toBeTruthy()
   })
 })
